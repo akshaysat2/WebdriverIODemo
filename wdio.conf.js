24858faa4af41980bcab207db37d8846606786b8
Akshay Sathian <akshaysat2@gmail.com>
2020-04-21 21:54:08 +0530

    added junit reporter

--- wdio.conf.js
+++ wdio.conf.js
@@ -127,7 +127,11 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
+    reporters: ['spec',
+    ['junit', {
+        outputDir: './'
+        }]
+    ],
     port: 4444,
 
     //
